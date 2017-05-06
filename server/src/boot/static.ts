754a2b79100b979ceffe5309157cab952769bbbb
Tienshiao Ma <tma@isbx.com>
2017-05-06 11:42:11 -0700

    Make client and server watch independent (addresses #4).

--- server/src/boot/static.ts
+++ server/src/boot/static.ts
@@ -4,24 +4,13 @@ import path = require('path');
 export = (server) => {
   let outDir = __dirname + '/../../../client/dist';
 
+  server.use(express.static(outDir));
+
   if (server.get('env') ===  'development') {
-    let webpackDevMiddleware = require('webpack-dev-middleware');
-    let webpack = require('webpack');
-    let config = require(__dirname + '/../../../config/webpack.development.js');
-
-    let compiler = webpack(config);
-    let middleware = webpackDevMiddleware(compiler, {
-      publicPath: config.output.publicPath
-    });
-
-    server.use(middleware);
-
     server.get('*', (req, res) => {
-      const index = middleware.fileSystem.readFileSync(path.join(config.output.path, 'index.html'));
-      res.end(index);
+      const index = path.join(outDir, 'index.html');
+      res.sendFile(index);
     });
-  } else {
-    server.use(express.static(outDir));
   }
 
 };
